f720430be7e478533f17bf0c20ca44cf3c2f39f6
micaelsbno <micaelsbno@gmail.com>
2018-07-17 22:29:13 +1000

    Added nba exercise / Movies Search

--- Micael/wk05/1-mon/main.rb
+++ Micael/wk05/1-mon/main.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'HTTParty'
 require 'pry'
+require 'FileUtils'
 
 get '/' do
   erb(:index)
@@ -9,9 +10,26 @@ end
 
 get '/movie' do
   @movie = HTTParty.get("http://www.omdbapi.com/?t=#{params[:movie_title].downcase}&apikey=2f6435d9")
-  if @movie['Response'] != 'False'
+  @scores = @movie['Ratings'].map{|source|source['Value']}
+  erb :movie
+end
+
+get '/search' do
+  
+  @search_title = params[:movie_title]
+  @movie = HTTParty.get("http://www.omdbapi.com/?t=#{@search_title.downcase}&apikey=2f6435d9")
+  @search = HTTParty.get("http://www.omdbapi.com/?s=#{@search_title.downcase}&apikey=2f6435d9")
+
+  File.open('./history/search_history.txt', 'a+') {|search| search.write("#{params[:movie_title]}\n")}
+
+  if @search['Response'] == 'False'
+    erb :movie
+  elsif @search['Search'].length == 1
     @scores = @movie['Ratings'].map{|source|source['Value']}
+    erb :movie
+  else
+    erb :search
   end
-  erb(:movie)
+
 end
 
