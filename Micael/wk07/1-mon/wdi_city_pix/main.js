f6bff9bffee2d83c6b69e3f9d1166bef33d1add6
micaelsbno <micaelsbno@gmail.com>
2018-07-31 11:05:23 +1000

    refactored wk07/1-mon homework

--- Micael/wk07/1-mon/wdi_city_pix/main.js
+++ Micael/wk07/1-mon/wdi_city_pix/main.js
@@ -1,29 +1,25 @@
 selector = document.getElementById('city-type')
-cities = ['NYC', 'SF', 'LA', 'SYD', 'ATX']
+var cities = {
+  NYC: 'nyc', 
+  SF: 'sf',
+  LA: 'la',
+  SYD: 'sydney',
+  ATX: 'austin'
+}
 
-cities.forEach(function(cityName){
+Object.keys(cities).forEach(function(cityName){
   var city = document.createElement('option')
   city.textContent = cityName
   selector.appendChild(city)
 })
 
-document.getElementById('city-type').addEventListener('change', function(event){
+body = document.querySelector('body');
 
-  if (event.target.value === 'NYC') {
-    document.querySelector('body').className = 'nyc'
+document.getElementById('city-type').addEventListener('change', function(event){
+  var city = cities[event.target.value]
+  if (city != undefined) {
+    body.className = city;
+  } else {
+    body.className = '';
   }
-  else if (event.target.value === 'SF'){
-    document.querySelector('body').className = 'sf'
-  }
-  else if (event.target.value === 'LA'){
-    document.querySelector('body').className = 'la'
-  }
-  else if (event.target.value === 'SYD'){
-    document.querySelector('body').className = 'sydney'
-  }
-  else if (event.target.value === 'ATX'){
-    document.querySelector('body').className = 'austin'
-  }
-  else
-    document.querySelector('body').className = ''
 })
